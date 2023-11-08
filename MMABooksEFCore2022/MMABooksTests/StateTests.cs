fa7a3f4c60f9981c8216dd4ff1b02d9931df4564
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-07 22:14:37 -0800

    Beginning Question 2 and Testing Each Method.

--- MMABooksEFCore2022/MMABooksTests/StateTests.cs
+++ MMABooksEFCore2022/MMABooksTests/StateTests.cs
@@ -27,15 +27,17 @@ namespace MMABooksTests
         [Test]
         public void GetAllTest()
         {
+            //This asserts that when retrieving data, there should be a total of 53 results for state name.
             states = dbContext.States.OrderBy(s => s.StateName).ToList();
-            Assert.AreEqual(52, states.Count);
+            Assert.AreEqual(53, states.Count);
             Assert.AreEqual("Alabama", states[0].StateName);
             PrintAll(states);
         }
 
         [Test]
         public void GetByPrimaryKeyTest()
-        {
+        {   //This code gets the state that has a code of OR
+            //Assert.AreEqual("Ore", s.StateName) means that the Primary Key "OR" is equivalent to "Ore"
             s = dbContext.States.Find("OR");
             Assert.IsNotNull(s);
             Assert.AreEqual("Ore", s.StateName);
@@ -45,6 +47,7 @@ namespace MMABooksTests
         [Test]
         public void GetUsingWhere()
         {
+            //First line gets the name of all states that start with an A; then outputting it to a list.
             states = dbContext.States.Where(s => s.StateName.StartsWith("A")).OrderBy(s => s.StateName).ToList();
             Assert.AreEqual(4, states.Count);
             Assert.AreEqual("Alabama", states[0].StateName);
@@ -54,6 +57,7 @@ namespace MMABooksTests
         [Test]
         public void GetWithCustomersTest()
         {
+            //Looking for Customers where the Statecode is OR
             s = dbContext.States.Include("Customers").Where(s => s.StateCode == "OR").SingleOrDefault();
             Assert.IsNotNull(s);
             Assert.AreEqual("Ore", s.StateName);
@@ -64,6 +68,7 @@ namespace MMABooksTests
         [Test]
         public void DeleteTest()
         {
+            //Save Changes is necessary
             s = dbContext.States.Find("HI");
             dbContext.States.Remove(s);
             dbContext.SaveChanges();
